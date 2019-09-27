9f7a53a608f7f7f348e270abe3d45a1671e1e36e
Dan Feeney <daniel.feeney@colorado.edu>
2019-09-27 10:16:50 -0600

    Catching iteration errors in stacking forces loop

--- TM_run.m
+++ TM_run.m
@@ -1,7 +1,7 @@
 %% Analysis of the treadmill data
 clear
 addpath('C:\Users\Daniel.Feeney\Documents\Trail Run\Run Code')
-data = importForcesTM('C:\Users\Daniel.Feeney\Dropbox (Boa)\Treadmill TMM\Data\WalkTest_9 - Report1.txt');
+data = importForcesTM('C:\Users\Daniel.Feeney\Dropbox (Boa)\Treadmill TMM\Data\WalkTest_15 - Report1.txt');
 
 %% Clean data for NaNs
 data.ForceZ(isnan(data.ForceZ))=0;
@@ -12,9 +12,9 @@ fq = 1000; %Sampling frequency
 fc = 20; %Cutoff frequency
 
 [b,a] = butter(2,fc/(fq/2),'low'); %second order BW filter, filtfilt doubles the order
-filt_forceZ = filtfilt(b,a,data.ForceZ(1:9999));
-filt_forceY = -1 .* filtfilt(b,a,data.ForceY(1:9999)); %This will be used for the M/L and A/P forces
-filt_forceX = filtfilt(b,a,data.ForceX(1:9999)); %This will be used for the M/L and A/P forces
+filt_forceZ = filtfilt(b,a,data.ForceZ);
+filt_forceY = -1 .* filtfilt(b,a,data.ForceY); %This will be used for the M/L and A/P forces
+filt_forceX = filtfilt(b,a,data.ForceX); %This will be used for the M/L and A/P forces
 
 filt_forceZ(filt_forceZ<0) = 0; %detrend the Z signals so they start at 0
 
@@ -38,13 +38,13 @@ filt_forceZ(filt_forceZ<0) = 0; %detrend the Z signals so they start at 0
 
 %% one side
 vertForce = zeros(floor(length(pks)/2),301); % preallocate
-APforce = zeros(floor(length(pks)/2)/2,301);
-MLforce = zeros(floor(length(pks)/2)/2,301);
+APforce = zeros(floor(length(pks)/2),301);
+MLforce = zeros(floor(length(pks)/2),301);
 for peak = 2:2:length(pks)
     tmp_location = locs(peak);
-    vertForce(peak/2,:) = filt_forceZ(tmp_location - 150 : tmp_location +150);   
-    APforce(peak/2,:) = filt_forceY(tmp_location - 150 : tmp_location +150); 
-    MLforce(peak/2,:) = filt_forceX(tmp_location - 150 : tmp_location +150); 
+    vertForce(floor(peak/2),:) = filt_forceZ(tmp_location - 150 : tmp_location +150);   
+    APforce(floor(peak/2),:) = filt_forceY(tmp_location - 150 : tmp_location +150); 
+    MLforce(floor(peak/2),:) = filt_forceX(tmp_location - 150 : tmp_location +150); 
 end
 
 figure(1)
@@ -59,17 +59,23 @@ shadedErrorBar(1:length(MLforce), MLforce, {@mean,@std}, 'lineprops','-r');
 
 %% Other side
 vertForce2 = zeros(floor(length(pks)/2),301); % preallocate
-APforce2 = zeros(floor(length(pks)/2)/2,301);
-MLforce2 = zeros(floor(length(pks)/2)/2,301);
+APforce2 = zeros(floor(length(pks)/2),301);
+MLforce2 = zeros(floor(length(pks)/2),301);
 for peak = 3:2:length(pks)
     tmp_location = locs(peak);
     tmp2 = floor(peak/2);
-    vertForce2(tmp2,:) = filt_forceZ(tmp_location - 150 : tmp_location +150);   
-    APforce2(tmp2,:) = filt_forceY(tmp_location - 150 : tmp_location +150); 
-    MLforce2(tmp2,:) = filt_forceX(tmp_location - 150 : tmp_location +150); 
+    try
+        vertForce2(tmp2,:) = filt_forceZ(tmp_location - 150 : tmp_location +150);   
+        APforce2(tmp2,:) = filt_forceY(tmp_location - 150 : tmp_location +150); 
+        MLforce2(tmp2,:) = filt_forceX(tmp_location - 150 : tmp_location +150); 
+    catch
+        fprintf('Reached end of matrix on iteration %s, skipped.\n',peak)
+    end
 end
 %% remove last row of 0's
 vertForce2(size(vertForce2,1),:) = [];
+MLforce2(size(MLforce2,1),:) = [];
+APforce2(size(APforce2,1),:) = [];
 % Change ML forces to respective side
 MLforce2 = -1 .* MLforce2;
 %% make plots
