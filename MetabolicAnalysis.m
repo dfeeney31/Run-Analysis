bce2326e4fc058e4cef6e6885ad993d37e6f57b5
Dan Feeney <daniel.feeney@colorado.edu>
2020-01-13 16:31:34 -0700

    Automating the output

--- MetabolicAnalysis.m
+++ MetabolicAnalysis.m
@@ -1,28 +1,69 @@
 %%%%%%% Metabolic data processing %%%%%%%
 %% import data with custom function
-MetData = importMetabolics('DF_uphill_pilot_092619.xlsx')
+clear
+%MetData = importMetabolics('D:\DF_uphill_pilot_092619.xlsx')
+MetData = importMetabolics('C:\Users\Daniel.Feeney\Dropbox (Boa)\Endurance Protocol Trail Run\DaveLongProtocolMetabolics\DGLongDurationSD.xlsx');
 % Find when marker data was not empty
 trials = find(MetData.Marker(4:end) ~= "")
-subWt = 69;
-%% Plot data
-plot(MetData.VO2Kg(trials(3):trials(4))) %LR
+subWt = 47.7;
+% Create a relative minute and second array
+MetData.tNew = datetime(MetData.t,'ConvertFrom','excel');
+%% Plot if no markers made
+plot(MetData.VO2Kg)
 hold on
-plot(MetData.VO2Kg(trials(6):trials(7))) %Standard lace
-plot(MetData.VO2Kg(trials(8):trials(9))) % overlapping panels
-plot(MetData.VO2Kg(trials(10):trials(11))) % Tri Panel
-plot(MetData.VO2Kg(trials(12):trials(13))) % X 
-plot(MetData.VO2Kg(trials(14):trials(15))) % Hierro
-legend('LR','Lace','OP','Tri','X','Hierro')
+plot(MetData.HR)
+hold off
+disp('Select start of three regions of interest')
+[start, loc] = ginput(3)
+
+figure(1)
+plot(MetData.VO2Kg(floor(start(1)):floor(start(1))+20),'Linewidth',2)
+hold on
+plot(MetData.VO2Kg(floor(start(2)):floor(start(2))+20),'Linewidth',2)
+plot(MetData.VO2Kg(floor(start(3)):floor(start(3))+20),'Linewidth',2)
+legend('1st Epoch', '2nd Epoch','3rd Epoch')
+ylabel('VO2 (ml O2/min/Kg)','FontSize',14)
+xlabel('Index','FontSize',14)
+
+figure(2)
+plot(MetData.HR(floor(start(1)):floor(start(1))+20),'Linewidth',2)
+hold on
+plot(MetData.HR(floor(start(2)):floor(start(2))+20),'Linewidth',2)
+plot(MetData.HR(floor(start(3)):floor(start(3))+20),'Linewidth',2)
+legend('1st Epoch', '2nd Epoch','3rd Epoch')
+ylabel('HR','FontSize',14)
+xlabel('Index','FontSize',14)
 
 %% Calculate metabolic rate with Brockway equation
-LR = ((16.58 * mean(MetData.VO2(trials(3):trials(3)+20))) + (4.51 * mean(MetData.VCO2(trials(3):trials(3)+20)))) / subWt;
-Lace = ((16.58 * mean(MetData.VO2(trials(7) - 20:trials(7)))) + (4.51 * mean(MetData.VCO2(trials(7)-20:trials(7))))) / subWt;
-OP = ((16.58 * mean(MetData.VO2(trials(9) - 20:trials(9)))) + (4.51 * mean(MetData.VCO2(trials(9)-20:trials(9))))) / subWt;
-Tri = ((16.58 * mean(MetData.VO2(trials(11) - 20:trials(11)))) + (4.51 * mean(MetData.VCO2(trials(11)-20:trials(11))))) / subWt;
-X = ((16.58 * mean(MetData.VO2(trials(13) - 20:trials(13)))) + (4.51 * mean(MetData.VCO2(trials(13)-20:trials(13))))) / subWt;
-results.Lace = Lace; results.LR = LR; results.OP = OP; results.Tri = Tri; results.X = X;
-%% Calc percent differences and store 
-pctDiff.LR = ((results.LR - results.Lace) / results.Lace) * 100;
-pctDiff.OP = ((results.OP - results.Lace) / results.Lace) * 100;
-pctDiff.Tri = ((results.Tri - results.Lace) / results.Lace) * 100;
-pctDiff.X = ((results.X - results.Lace) / results.Lace) * 100;
+firstEpoch = ((16.58 * mean(MetData.VO2(floor(start(1)):floor(start(1))+20))) + (4.51 * mean(MetData.VCO2(floor(start(1)):floor(start(1))+20)))) / subWt
+secondEpoch = ((16.58 * mean(MetData.VO2(floor(start(2)):floor(start(2))+20))) + (4.51 * mean(MetData.VCO2(floor(start(2)):floor(start(2))+20)))) / subWt
+thirdEpoch = ((16.58 * mean(MetData.VO2(floor(start(3)):floor(start(3))+20))) + (4.51 * mean(MetData.VCO2(floor(start(3)):floor(start(3))+20)))) / subWt
+
+avgHR1 = mean(MetData.HR((floor(start(1)):floor(start(1))+20)))
+avgHR2 = mean(MetData.HR((floor(start(2)):floor(start(2))+20)))
+avgHR3 = mean(MetData.HR((floor(start(3)):floor(start(3))+20)))
+
+output = [firstEpoch,avgHR1;secondEpoch,avgHR2;thirdEpoch, avgHR3]
+
+%% Plot data if markers were added
+% plot(MetData.VO2Kg(trials(3):trials(4))) %LR
+% hold on
+% plot(MetData.VO2Kg(trials(6):trials(7))) %Standard lace
+% plot(MetData.VO2Kg(trials(8):trials(9))) % overlapping panels
+% plot(MetData.VO2Kg(trials(10):trials(11))) % Tri Panel
+% plot(MetData.VO2Kg(trials(12):trials(13))) % X 
+% plot(MetData.VO2Kg(trials(14):trials(15))) % Hierro
+% legend('LR','Lace','OP','Tri','X','Hierro')
+% 
+% %% Calculate metabolic rate with Brockway equation
+% LR = ((16.58 * mean(MetData.VO2(trials(3):trials(3)+20))) + (4.51 * mean(MetData.VCO2(trials(3):trials(3)+20)))) / subWt;
+% Lace = ((16.58 * mean(MetData.VO2(trials(7) - 20:trials(7)))) + (4.51 * mean(MetData.VCO2(trials(7)-20:trials(7))))) / subWt;
+% OP = ((16.58 * mean(MetData.VO2(trials(9) - 20:trials(9)))) + (4.51 * mean(MetData.VCO2(trials(9)-20:trials(9))))) / subWt;
+% Tri = ((16.58 * mean(MetData.VO2(trials(11) - 20:trials(11)))) + (4.51 * mean(MetData.VCO2(trials(11)-20:trials(11))))) / subWt;
+% X = ((16.58 * mean(MetData.VO2(trials(13) - 20:trials(13)))) + (4.51 * mean(MetData.VCO2(trials(13)-20:trials(13))))) / subWt;
+% results.Lace = Lace; results.LR = LR; results.OP = OP; results.Tri = Tri; results.X = X;
+% %% Calc percent differences and store 
+% pctDiff.LR = ((results.LR - results.Lace) / results.Lace) * 100;
+% pctDiff.OP = ((results.OP - results.Lace) / results.Lace) * 100;
+% pctDiff.Tri = ((results.Tri - results.Lace) / results.Lace) * 100;
+% pctDiff.X = ((results.X - results.Lace) / results.Lace) * 100;
--- TM_run.m
+++ TM_run.m
@@ -1,9 +1,9 @@
 %% Analysis of the treadmill data
 clear
-subBW = 74;
+subBW =72;
 addpath('C:\Users\Daniel.Feeney\Documents\Trail Run\Run Code')
 %data = importForcesTM('C:\Users\Daniel.Feeney\Dropbox (Boa)\Treadmill TMM\Data\WalkTest_30 - Report1.txt');
-data = importForcesTM('C:\Users\Daniel.Feeney\Dropbox (Boa)\Trail Run Internal Pilot\HierroTesting_Nov2019\TM Data\DevinLevelDD - Report1.txt');
+data = importForcesTM('C:\Users\Daniel.Feeney\Dropbox (Boa)\Endurance Protocol Trail Run\DaveLongProtocolTM\DaveSingle32min - Report1.txt');
 %tmp_metadata = strsplit(convertCharsToStrings(file.name),' ');
 %% Clean data for NaNs
 data.ForceZ(isnan(data.ForceZ))=0;
@@ -19,8 +19,9 @@ filt_forceZ = filtfilt(b,a,data.ForceZ);
 filt_forceY = filtfilt(b,a,data.ForceY); %This will be used for the M/L and A/P forces
 filt_forceX = -1 .* filtfilt(b,a,data.ForceX); %This will be used for the M/L and A/P forces
 
-filt_forceZ(filt_forceZ<0) = 0; %detrend the Z signals so they start at 0
-
+%filt_forceZ = filt_forceZ-35;
+filt_forceZ(filt_forceZ<10) = 0; %detrend the Z signals so they start at 0
+%plot(filt_forceZ)
 %% Find peaks in force and concatenate
 [pks,locs] = findpeaks(filt_forceZ, 'minpeakdistance', 250, 'MinPeakProminence',100); %Find locations and values of peaks
 
@@ -43,16 +44,16 @@ for peak = 2:2:length(pks)
     end
 end
 
-figure(1)
-title('Z force')
-shadedErrorBar(1:length(vertForce), vertForce, {@mean,@std}, 'lineprops','-r');
-figure(2)
-title('AP force')
-shadedErrorBar(1:length(APforce), APforce, {@mean,@std}, 'lineprops','-r');
-figure(3)
-title('ML force')
-shadedErrorBar(1:length(MLforce), MLforce, {@mean,@std}, 'lineprops','-r');
-ylabel('Medial is negative')
+% figure(1)
+% title('Z force')
+% shadedErrorBar(1:length(vertForce), vertForce, {@mean,@std}, 'lineprops','-r');
+% figure(2)
+% title('AP force')
+% shadedErrorBar(1:length(APforce), APforce, {@mean,@std}, 'lineprops','-r');
+% figure(3)
+% title('ML force')
+% shadedErrorBar(1:length(MLforce), MLforce, {@mean,@std}, 'lineprops','-r');
+% ylabel('Medial is negative')
 
 
 %% calculate loading rates and peak force values
@@ -60,7 +61,7 @@ ylabel('Medial is negative')
 for step = 1:size(vertForce,1)
     tmp_step = vertForce(step,:);
     count = 1;
-    for ind = 1:100
+    for ind = 1:120
         if (tmp_step(ind) == 0 & tmp_step(ind + 1) > 0)
             trueZeros(step,count) = ind;
             count = count + 1;
@@ -74,18 +75,24 @@ rates = zeros(1,length(trueZeros));
 pks_vert = zeros(1,length(trueZeros)); pks_vert = pks_vert ./ subBW; pks_vert = pks_vert';
 pks_med = zeros(1,length(trueZeros)); pks_med = pks_med';
 
-%% heuristic is find the first impact peak, find 20 and 80% of it, use those indices to calculate the forces and RFD
+%% heuristic for VLRis find the first impact peak (currently approximated  
+% as t_step indices (set above) beyond the landing), find 20 and 80% of of that value 
+% (find fxn), use those indices to calculate the forces and RFD. Could 
+% automate finding first impact peak better. 
+
 for i = 1:(length(trueZeros))
-    tmpIndex = trueZeros(i);
-    peakTmp = vertForce(i,trueZeros(i)+t_step);
-    twentyPct = find(vertForce(i,trueZeros(i):trueZeros(i)+100) > 0.2 * peakTmp ,1) + tmpIndex;
-    eightyPct = find(vertForce(i,trueZeros(i):trueZeros(i)+100) > 0.8 * peakTmp ,1) + tmpIndex;
-    rates(i) =  (vertForce(i,eightyPct) - vertForce(i,twentyPct)) / ((eightyPct - twentyPct)/1000);
-    pks_vert(i) = max(vertForce(i,:));
-    pks_med(i) = max(-1 .* MLforce(i,:));
-    pks_lat(i) = max(MLforce(i,:));
-    pks_brake(i) = max(-1 .* APforce(i,:));
-    pks_prop(i) = max(APforce(i,:));
+    try
+        tmpIndex = trueZeros(i);
+        peakTmp = vertForce(i,trueZeros(i)+t_step);
+        twentyPct = find(vertForce(i,trueZeros(i):trueZeros(i)+100) > 0.2 * peakTmp ,1) + tmpIndex;
+        eightyPct = find(vertForce(i,trueZeros(i):trueZeros(i)+100) > 0.8 * peakTmp ,1) + tmpIndex;
+        rates(i) =  (vertForce(i,eightyPct) - vertForce(i,twentyPct)) / ((eightyPct - twentyPct)/1000);
+        pks_vert(i) = max(vertForce(i,:));
+        pks_med(i) = max(-1 .* MLforce(i,:));
+        pks_lat(i) = max(MLforce(i,:));
+        pks_brake(i) = max(-1 .* APforce(i,:));
+        pks_prop(i) = max(APforce(i,:));
+    end
 end
 rates = rates';
 rates = rates ./9.81; rates = rates ./ subBW;
@@ -124,12 +131,49 @@ APforce2(size(APforce2,1),:) = [];
 % Change ML forces to respective side
 %MLforce2 = -1 .* MLforce2;
 %% make plots
+% figure(4)
+% title('Z force')
+% shadedErrorBar(1:length(vertForce2), vertForce2, {@mean,@std}, 'lineprops','-b');
+% figure(5)
+% title('AP force')
+% shadedErrorBar(1:length(APforce2), APforce2, {@mean,@std}, 'lineprops','-b');
+% figure(6)
+% title('ML force')
+% shadedErrorBar(1:length(MLforce2), -1 .* MLforce2, {@mean,@std}, 'lineprops','-b');
+
+
+%% Symmetry plots
 figure(4)
 title('Z force')
 shadedErrorBar(1:length(vertForce2), vertForce2, {@mean,@std}, 'lineprops','-b');
+hold on
+shadedErrorBar(1:length(vertForce), vertForce, {@mean,@std}, 'lineprops','-r');
 figure(5)
 title('AP force')
-shadedErrorBar(1:length(APforce2), APforce2, {@mean,@std}, 'lineprops','-b');
+shadedErrorBar(1:length(APforce2), -1 .* APforce2, {@mean,@std}, 'lineprops','-b');
+hold on
+shadedErrorBar(1:length(APforce),  -1 .*  APforce, {@mean,@std}, 'lineprops','-r');
 figure(6)
 title('ML force')
 shadedErrorBar(1:length(MLforce2), -1 .* MLforce2, {@mean,@std}, 'lineprops','-b');
+hold on
+shadedErrorBar(1:length(MLforce), MLforce, {@mean,@std}, 'lineprops','-r');
+
+
+%%% scratch work for plotting two loading rate time series against each
+%%% other
+% figure; hold on;
+% h(1) = shadedErrorBar(1:length(vertForce_DD), vertForce_DD, {@mean,@std}, 'lineprops','-b');
+% title('Vertical force level running')
+% h(2) = shadedErrorBar(1:length(vertForce_Lace), vertForce_Lace, {@mean,@std}, 'lineprops','-r');
+% legend([h(1).mainLine h(2).mainLine], 'DD', 'Lace')
+% xlabel('Time (1/1000)s','FontSize',14,'FontWeight','bold')
+% ylabel('Force (N)','FontSize',14,'FontWeight','bold')
+% 
+% figure; hold on;
+% h(1) = shadedErrorBar(1:length(APforce_DD), -1 .* APforce_DD, {@mean,@std}, 'lineprops','-b');
+% title('AP force level running')
+% h(2) = shadedErrorBar(1:length(APforce_Lace), -1 .* APforce_Lace, {@mean,@std}, 'lineprops','-r');
+% legend([h(1).mainLine h(2).mainLine], 'DD', 'Lace')
+% xlabel('Time (1/1000)s','FontSize',14,'FontWeight','bold')
+% ylabel('Force (N)','FontSize',14,'FontWeight','bold')
